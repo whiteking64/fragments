e184e34440820846ab7bed00904bfc1f5061cb90
whiteking64 <whiteking64@gmail.com>
2021-08-31 12:15:30 +0000

    Create main function

--- quantize_mobilenetv3_small.py
+++ quantize_mobilenetv3_small.py
@@ -1,9 +1,7 @@
-import os
-import time
-
 import torch
 import torch.nn as nn
 import torch.quantization
+import torchvision
 from torch import Tensor
 from torch.quantization import QuantStub, DeQuantStub, fuse_modules
 from torchvision import datasets
@@ -13,7 +11,6 @@ from torchvision.models.mobilenetv3 import (
     MobileNetV3,
     SqueezeExcitation,
     _mobilenet_v3_conf,
-    mobilenet_v3_small,
 )
 from torchvision.models.quantization.utils import _replace_relu
 import torchvision.transforms as transforms
@@ -69,9 +66,6 @@ class QuantizableMobileNetV3(MobileNetV3):
                 fuse_modules(m, modules_to_fuse, inplace=True)
             elif type(m) == QuantizableSqueezeExcitation:
                 m.fuse_model()
-
-
-# 2. Helper functions
 
 
 class AverageMeter(object):
@@ -164,8 +158,7 @@ def evaluate(model, data_loader, neval_batches, device="cpu"):
     return top1.avg, top5.avg
 
 
-def load_model(model_file):
-    arch = "mobilenet_v3_small"
+def load_model(model_file, arch):
     inverted_residual_setting, last_channel = _mobilenet_v3_conf(arch)
     model = QuantizableMobileNetV3(
         inverted_residual_setting,
@@ -178,146 +171,76 @@ def load_model(model_file):
     return model
 
 
-def print_size_of_model(model):
-    torch.save(model.state_dict(), "temp.p")
-    print("Size (MB):", os.path.getsize("temp.p") / 1e6)
-    os.remove("temp.p")
+def main():
+    # arch = "mobilenet_v3_small"
+    arch = "mobilenet_v3_large"
+    data_path = "/home/val"
 
+    eval_batch_size = 50  # 50000 images in total
+    num_eval_batches = 1000
 
-# 3. Define dataset and data loaders
-
-data_path = "/home/val"
-saved_model_dir = "/home/_gen"
-float_model_file = "/home/onnx_pretrained_models/mobilenet_v3_small-047dcff4.pth"
-scripted_quantized_model_file = os.path.join(
-    saved_model_dir, "mobilenet_quantization_scripted_quantized.pth"
-)
-
-eval_batch_size = 50  # 50000 images in total
-num_eval_batches = 1000
-
-normalize = transforms.Normalize(
-    mean=[0.485, 0.456, 0.406],
-    std=[0.229, 0.224, 0.225],
-)
-val_loader = torch.utils.data.DataLoader(
-    datasets.ImageFolder(
-        data_path,
-        transforms.Compose(
-            [
-                transforms.Resize(256),
-                transforms.CenterCrop(224),
-                transforms.ToTensor(),
-                normalize,
-            ]
+    normalize = transforms.Normalize(
+        mean=[0.485, 0.456, 0.406],
+        std=[0.229, 0.224, 0.225],
+    )
+    val_loader = torch.utils.data.DataLoader(
+        datasets.ImageFolder(
+            data_path,
+            transforms.Compose(
+                [
+                    # transforms.Resize(256),
+                    # transforms.CenterCrop(224),
+                    transforms.ToTensor(),
+                    normalize,
+                ]
+            ),
         ),
-    ),
-    batch_size=eval_batch_size,
-    shuffle=False,
-    num_workers=1,
-    pin_memory=True,
-)
-
-# print("Float model evaluation")
-# float_model = mobilenet_v3_small()
-# float_model.load_state_dict(torch.load(float_model_file))
-# float_model.cuda()
-# top1, top5 = evaluate(float_model, val_loader, num_eval_batches, "cuda:0")
-# print(
-#     "Evaluation accuracy on {n} images\nAcc@1 {top1:.3f} Acc@5 {top5:.3f}".format(
-#         n=num_eval_batches * eval_batch_size, top1=top1, top5=top5
-#     )
-# )
-
-
-# 4. Post-training static quantization
-# ------------------------------------
-
-num_calib_batches = 1000
-
-myModel = load_model(float_model_file).to("cpu")
-_replace_relu(myModel)
-myModel.eval()
-
-myModel.fuse_model()
-
-# Specify quantization configuration
-# Start with simple min/max range estimation and
-# per-tensor quantization of weights
-myModel.qconfig = torch.quantization.default_qconfig
-print(myModel.qconfig)
-torch.quantization.prepare(myModel, inplace=True)
-
-# Calibrate with the val set (should be train set ideally)
-evaluate(myModel, val_loader, neval_batches=num_calib_batches)
-
-# Convert to quantized model
-torch.quantization.convert(myModel, inplace=True)
-print("Post Training Quantization: Convert done")
-
-top1, top5 = evaluate(myModel, val_loader, neval_batches=num_eval_batches)
-print(
-    "Evaluation accuracy on {n} images\nAcc@1 {top1:.3f} Acc@5 {top5:.3f}".format(
-        n=num_eval_batches * eval_batch_size, top1=top1, top5=top5
+        batch_size=eval_batch_size,
+        shuffle=False,
+        num_workers=1,
+        pin_memory=True,
     )
-)
 
-
-# In addition, we can significantly improve on the accuracy simply by using a
-# different quantization configuration. We repeat the same exercise with the
-# recommended configuration for quantizing for x86 architectures.
-# This configuration does the following:
-
-# - Quantizes weights on a per-channel basis
-# - Uses a histogram observer that collects a histogram of activations and
-# then picks quantization parameters in an optimal manner.
-
-per_channel_quantized_model = load_model(float_model_file)
-_replace_relu(per_channel_quantized_model)
-per_channel_quantized_model.eval()
-per_channel_quantized_model.fuse_model()
-per_channel_quantized_model.qconfig = torch.quantization.get_default_qconfig("fbgemm")
-print(per_channel_quantized_model.qconfig)
-
-torch.quantization.prepare(per_channel_quantized_model, inplace=True)
-evaluate(per_channel_quantized_model, val_loader, num_calib_batches)
-torch.quantization.convert(per_channel_quantized_model, inplace=True)
-top1, top5 = evaluate(
-    per_channel_quantized_model,
-    val_loader,
-    neval_batches=num_eval_batches,
-)
-print(
-    "Evaluation accuracy on {n} images\nAcc@1 {top1:.3f} Acc@5 {top5:.3f}".format(
-        n=num_eval_batches * eval_batch_size, top1=top1, top5=top5
+    print("Float model evaluation")
+    float_model = eval("torchvision.models." + arch)(pretrained=True)
+    # float_model.load_state_dict(torch.load(float_model_file))
+    float_model.cuda()
+    top1, top5 = evaluate(float_model, val_loader, num_eval_batches, "cuda:0")
+    print(
+        "Evaluation accuracy on {n} images\nAcc@1 {top1:.3f} Acc@5 {top5:.3f}".format(
+            n=num_eval_batches * eval_batch_size, top1=top1, top5=top5
+        )
     )
-)
 
-# torch.jit.save(
-#     torch.jit.script(per_channel_quantized_model),
-#     os.path.join(saved_model_dir, scripted_quantized_model_file),
-# )
+    # 4. Post-training static quantization
+    # ------------------------------------
+
+    num_calib_batches = num_eval_batches
+
+    float_model_file = "/home/onnx_pretrained_models/mobilenet_v3_large-8738ca79.pth"
+    per_channel_quantized_model = load_model(float_model_file, arch)
+    _replace_relu(per_channel_quantized_model)
+    per_channel_quantized_model.eval()
+    per_channel_quantized_model.fuse_model()
+    per_channel_quantized_model.qconfig = torch.quantization.get_default_qconfig(
+        "qnnpack"
+    )
+    print(per_channel_quantized_model.qconfig)
+
+    torch.quantization.prepare(per_channel_quantized_model, inplace=True)
+    evaluate(per_channel_quantized_model, val_loader, num_calib_batches)
+    torch.quantization.convert(per_channel_quantized_model, inplace=True)
+    top1, top5 = evaluate(
+        per_channel_quantized_model,
+        val_loader,
+        neval_batches=num_calib_batches,
+    )
+    print(
+        "Evaluation accuracy on {n} images\nAcc@1 {top1:.3f} Acc@5 {top5:.3f}".format(
+            n=eval_batch_size * num_calib_batches, top1=top1, top5=top5
+        )
+    )
 
 
-def run_benchmark(model_file, img_loader):
-    elapsed = 0
-    model = torch.jit.load(model_file)
-    model.eval()
-    num_batches = 5
-    # Run the scripted model on a few batches of images
-    for i, (images, target) in enumerate(img_loader):
-        if i < num_batches:
-            start = time.time()
-            _ = model(images)
-            end = time.time()
-            elapsed = elapsed + (end - start)
-        else:
-            break
-    num_images = images.size()[0] * num_batches
-
-    print("Elapsed time: %3.0f ms" % (elapsed / num_images * 1000))
-    return elapsed
-
-
-# run_benchmark(os.path.join(saved_model_dir, scripted_float_model_file), val_loader)
-# run_benchmark(os.path.join(saved_model_dir, scripted_quantized_model_file), val_loader)
+if __name__ == "__main__":
+    main()
